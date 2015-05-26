0a7729e4830370b3ed19a6d0e087d2f199a316c0
BigHulk <bigonemailbox@gmail.com>
2015-05-26 10:49:20 +0800

    edit from remote for test pull

--- app/src/main/java/com/example/administrator/helloword/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloword/MainActivity.java
@@ -48,3 +48,4 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 // Add Two For Test From Local
 // edit local push origin for test
 // edit from remote pull origin for test
+// edit from remote for test pull
